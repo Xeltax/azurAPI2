1d310e69eac5c4aeb26780f6d2cbe3bfe882f876
Ewennn <ewen.bosquet@gmail.com>
2024-12-03 14:21:53 +0100

    secure/ Texts functions

--- src/functions/GetText.js
+++ src/functions/GetText.js
@@ -1,5 +1,6 @@
 const { app } = require('@azure/functions');
 const { getContainer } = require('../shared/database');
+const { verify_JWT } = require('../shared/jwt');
 
 // Initialize the Cosmos DB container for texts
 const textsContainer = getContainer('texts');
@@ -8,6 +9,15 @@ app.http('GetTexts', {
     methods: ['GET'],
     authLevel: 'anonymous',
     handler: async (request, context) => {
+
+        const JWT_verification = verify_JWT(request)
+        if (JWT_verification.status !== 200) {
+            return {
+                status: JWT_verification.status,
+                body: JWT_verification.body
+            };
+        }
+
         try {
             // Fetch all items from the Cosmos DB container
             const { resources: texts } = await textsContainer.items.readAll().fetchAll();
